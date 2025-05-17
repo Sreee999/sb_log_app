11d9f2ba0530cdea43b71d90842c0f97135ecb84
Sreee999 <edify99sree@gmail.com>
2025-05-18 00:01:58 +0530

    Removing Unused variables
    Signed-off-by: Sreee999 <edify99sree@gmail.com>

--- src/main/java/in/ashokit/ServletInitializer.java
+++ src/main/java/in/ashokit/ServletInitializer.java
@@ -8,25 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SbRestAppApplication.class);
-		int num = 10; 
-
-		int num2 = 20;
-
-		String name = "Sreee";
 		
-		int num3 = 30; 
-
-		boolean status = false;
-		int num10 = 1000; 
-		double sal = 998875.78; 
-
-		String university = "S V University"; 
-		int num5 = 100; 
-
-		int num25 = 25; 
-
-		int num50 = 50;
-
 
 	}
 
